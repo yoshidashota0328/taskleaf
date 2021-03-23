14da101bcbdb0cd3686edf36756bfc952cba337d
yoshidashota0328 <60382976+yoshidashota0328@users.noreply.github.com>
2021-03-23 21:30:44 +0900

    3/23

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,17 +12,35 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク『#{task.name}』を登録しました。"
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク『#{@task.name}』を登録しました。"
+    else
+      render :new
+    end
   end
+
+  def update 
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク『#{task.name}』を更新しました。"
+  end
+  
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク『#{task.name}』を削除しました。"
+    end
 
   private 
 
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_072945) do
+ActiveRecord::Schema.define(version: 2021_03_22_160944) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
