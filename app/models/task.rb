d997541b06545132cff3b0018107fcfd5e01eb99
yoshidashota0328 <60382976+yoshidashota0328@users.noreply.github.com>
2021-03-27 01:15:02 +0900

    3/26

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,18 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   private
 
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
+
+
 end
